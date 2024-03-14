6924effb9a5c75aead9bad1d8bdaadbd7bb42034
0918nobita <nobita.0918@gmail.com>
2024-03-14 19:10:17 +0900

    Index ページを追加した

---
+++ app/routes/_index.tsx
@@ -0,0 +1,17 @@
+export default function Index() {
+    return (
+        <p id="index-page">
+            This is a demo for Remix.
+            <br />
+            Check out{' '}
+            <a
+                href="https://remix.run"
+                rel="noopener noreferrer"
+                target="_blank"
+            >
+                the docs at remix.run
+            </a>
+            .
+        </p>
+    );
+}
